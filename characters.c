ea67518dc590444deff03eac524c932712ee1b6f
Eric Bucheit <ebucheit@e1z2r8p36.42.us.org>
2017-05-30 21:02:12 -0700

    test

--- characters.c
+++ characters.c
@@ -35,30 +35,6 @@ void	print_char(va_list ap, t_flags flags)
 	ft_putchar(c);
 }
 
-void	putwide(wchar_t chr)
-{
-	if (chr <= 0x7F)
-		ft_putchar(chr);
-	else if (chr <= 0x7FF)
-	{
-		ft_putchar((chr >> 6) + 0xC0);
-		ft_putchar((chr & 0x3F) + 0x80);
-	}
-	else if (chr <= 0xFFFF)
-	{
-		ft_putchar((chr >> 12) + 0xE0);
-		ft_putchar(((chr >> 6) & 0x3F) + 0x80);
-		ft_putchar((chr & 0x3F) + 0x80);
-	}
-	else if (chr <= 0x10FFFF)
-	{
-		ft_putchar((chr >> 18) + 0xF0);
-		ft_putchar(((chr >> 12) & 0x3F) + 0x80);
-		ft_putchar(((chr >> 6) & 0x3F) + 0x80);
-		ft_putchar((chr & 0x3F) + 0x80);
-	}
-}
-
 void	chars(va_list ap, t_flags flags, char specifier)
 {
 	if (specifier == 'c' && flags.l == 0)
@@ -66,6 +42,6 @@ void	chars(va_list ap, t_flags flags, char specifier)
 	else
 	{
 		prepend_width(flags, flags.width);
-		putwide(va_arg(ap, wint_t));
+		ft_putstr(wide_to_s(va_arg(ap, wint_t)));
 	}
 }
