700b2c50b73f2cdd8aa016f41a8990065411b293
Eric Bucheit <ebucheit@e1z2r5p3.42.us.org>
2017-05-31 18:18:33 -0700

    testing

--- characters.c
+++ characters.c
@@ -26,21 +26,17 @@ void	prepend_width(t_flags flags, int width)
 		ft_putchar(spaces);
 }
 
-void	print_char(va_list ap, t_flags flags)
-{
-	char c;
-
-	c = va_arg(ap, int);
-	prepend_width(flags, flags.width);
-	ft_putchar(c);
-}
-
 void	chars(va_list ap, t_flags flags, char specifier)
 {
 	char *ws;
+	char c;
 
 	if (specifier == 'c' && flags.l == 0)
-		print_char(ap, flags);
+	{
+		c = va_arg(ap, int);
+		prepend_width(flags, flags.width);
+		ft_putchar(c);
+	}
 	else
 	{
 		prepend_width(flags, flags.width);
